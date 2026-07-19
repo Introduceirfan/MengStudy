86408ad1845338901208426d477af44c27e63c5b
Ini Irfan <febriannaufalirfansyah@gmail.com>
2026-07-19 15:27:18 +0700

    im level 15 now. the grinding still otw

--- Python RPG/level 10-20.py
+++ Python RPG/level 10-20.py
@@ -35,3 +35,74 @@
 # if __name__ == "__main__":
 #     print(cari_indeks_huruf("jakarta", "k"))
 #     print(cari_indeks_huruf("jakarta", "z"))
+
+#4 level : 11, membalikkan kata
+# def reverse(string):
+#     result = ""
+#     for i in string:
+#         result = i + result
+#     return result
+
+# if __name__ == "__main__":
+#     print(reverse("kopik"))
+
+#5 level: 12, does it palindrome?
+# def reverse(string):
+#     result = ""
+#     for i in string:
+#         result = i + result
+#     return result
+
+# def is_palindrome(string):
+#     reversed = reverse(string)
+#     if string == reversed:
+#         return True
+#     return False
+
+# if __name__ == "__main__":
+#     print(is_palindrome("katak"))
+
+#6 level: 13, is it triangle?
+# def triangle(n):
+#     i = 1
+#     stars = "*"
+#     while i <= n:
+#         print(f"{stars * i}")
+#         i += 1
+
+# if __name__ == "__main__":
+#     triangle(4)
+
+#7 level: 13, still is it triangle(reversed)?
+# def triangle(n):
+#     i = n
+#     while i > 0:
+#         print("*" * i)
+#         i -= 1
+
+# if __name__ == "__main__":
+#     triangle(4)
+
+#8 level: 14, still triangle (but right margin)
+# def triangle(n):
+#     i = 1
+#     space = " "
+#     star = "*"
+#     while i <= n:
+#         print(f"{space * (n-i)}{star * i}")
+#         i += 1
+
+# if __name__ == "__main__":
+#     triangle(4)
+
+#9 level: 15, triangle but more hardway :) this is spruce by the way.
+# def triangle(n):
+#     i = 1
+#     space = " "
+#     star = "*"
+#     while i <= n:
+#         print(f"{space * (n-i)}{star * ((2*i)-1)}")
+#         i += 1
+#     print(f"{space * (n-1)}{star * ((2*1)-1)}")
+# if __name__ == "__main__":
+#     triangle(4)
