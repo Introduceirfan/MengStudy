c1c9fab189a93998ad46542b5940fd301a75fc14
Ini Irfan <febriannaufalirfansyah@gmail.com>
2026-07-26 23:29:23 +0700

    not much

--- Python/Helsinski/part06-02_fruit_market/src/fruit_market.py
+++ Python/Helsinski/part06-02_fruit_market/src/fruit_market.py
@@ -1,1 +1,11 @@
 # write your solution here
+def read_fruits():
+    with open("fruits.csv") as new_file:
+        fruits_list = {}
+        for line in new_file:
+            line = line.replace("\n", "")
+            parts = line.split(";")
+            fruits_list[parts[0]] = float(parts[1])
+        return fruits_list
+if __name__ == "__main__":
+    print(read_fruits())
--- Python/Helsinski/part06-03_matrix/src/matrix.py
+++ Python/Helsinski/part06-03_matrix/src/matrix.py
@@ -1,1 +1,19 @@
 # write your solution here
+def read_matrix():
+    with open("matrix.txt") as new_file:
+        matrix_list = []
+        for line in new_file:
+            parts = line.split(",")
+            matrix_list.append(parts)
+        return matrix_list
+
+def matrix_sum():
+    matrix = read_matrix()
+    sum = 0
+    for row in matrix:
+        sum += row
+    return matrix 
+    
+if __name__ == "__main__":
+    
+    print(read_matrix())
