6ee2771b32115d18fb8f8824db2b0ded4c325fab
Ini Irfan <febriannaufalirfansyah@gmail.com>
2026-07-30 07:45:36 +0700

    uhh not too much/...

--- Python/Helsinski/part06-03_matrix/src/matrix.py
+++ Python/Helsinski/part06-03_matrix/src/matrix.py
@@ -3,17 +3,35 @@ def read_matrix():
     with open("matrix.txt") as new_file:
         matrix_list = []
         for line in new_file:
-            parts = line.split(",")
-            matrix_list.append(parts)
+            parts = line.strip().split(",")
+            row = []
+            for matrax in parts:
+                row.append(int(matrax))
+            matrix_list.append(row)
         return matrix_list
 
 def matrix_sum():
     matrix = read_matrix()
-    sum = 0
+    list_sum = []
     for row in matrix:
-        sum += row
-    return matrix 
-    
+        list_sum.append(sum(row))
+    return sum(list_sum) 
+
+def matrix_max():
+    matrix = read_matrix()
+    list_max = []
+    for row in matrix:
+        list_max.append(max(row))
+    return max(list_max)
+
+def row_sums():
+    matrix = read_matrix()
+    list_row_sum = []
+    for row in matrix:
+        list_row_sum.append(sum(row))
+    return list_row_sum
+
 if __name__ == "__main__":
-    
-    print(read_matrix())
+    print(matrix_sum())
+    print(matrix_max())
+    print(row_sums())
--- Python/Helsinski/part06-09_city_bikes/src/city_bikes.py
+++ Python/Helsinski/part06-09_city_bikes/src/city_bikes.py
@@ -1,2 +1,36 @@
 # tee ratkaisu tänne
 # Write your solution here
+import math
+
+def get_station_data(files):
+    station = {}
+    with open(files) as new_files:
+        for line in new_files:
+            parts = line.split(';')
+            if parts[0] == "Longitude":
+                continue
+            station[parts[3]] = (float(parts[0]), float(parts[1]))
+    return station
+
+def distance(stations: dict, station1: str, station2: str):
+
+    longitude1, latitude1 = stations[station1]
+    longitude2, latitude2 = stations[station2]
+
+    x_km = (longitude1 - longitude2) * 55.26
+    y_km = (latitude1 - latitude2) * 111.2
+    distance_km = math.sqrt(x_km**2 + y_km**2)
+
+    return distance_km
+
+def greatest_distance(stey):
+    max_distance = 0
+    stat1 = ""
+    stat2 = ""
+    for item in stey:
+        
+
+if __name__ == "__main__":
+    stations = get_station_data('stations1.csv')
+    station1, station2, greatest = greatest_distance(stations)
+    print(station1, station2, greatest)
