c25f2fb8e173e5a59d7e05f3cfc9d15a9ad5c8c2
Ini Irfan <febriannaufalirfansyah@gmail.com>
2026-07-31 18:42:23 +0700

    2 problems before interviews.

--- Python/Helsinski/part06-10_inscription/src/inscription.py
+++ Python/Helsinski/part06-10_inscription/src/inscription.py
@@ -1,1 +1,7 @@
 # Write your solution here
+def inscription():
+    sign = input("Whom should I sign this to: ")
+    save = input("Where shall I save it: ")
+    with open(save, "w") as new_file:
+        new_file.write(f"Hi {sign}, we hope you enjoy learning Python with us! Best, Mooc.fi Team")
+inscription()
--- Python/Helsinski/part06-11_diary/src/diary.py
+++ Python/Helsinski/part06-11_diary/src/diary.py
@@ -1,1 +1,22 @@
 # Write your solution here
+def diary():
+    while True:
+        print("1 - add an entry, 2 - read entries, 0 - quit")
+        input_func = int(input("Function: "))
+
+        if input_func == 1:
+            diary_input = input("Diary entry: ")
+            with open("diary.txt", "a") as my_file:
+                my_file.write(f"{diary_input}\n")
+            print("Diary saved")
+
+        elif input_func == 2:
+            print("Entries: ")
+            with open("diary.txt", "r") as new_file:
+                for line in new_file:
+                    parts = line.strip()
+                    print(parts)
+        else:
+            print("Bye now!")
+            break
+diary()
