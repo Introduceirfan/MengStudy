870b0d7f5148453985acc26bd8ad1ba271866836
Ini Irfan <febriannaufalirfansyah@gmail.com>
2026-08-01 19:13:45 +0700

    small progress for this holidey

--- Python/Helsinski/part06-12_filtering_file_contents/src/filtering_file_contents.py
+++ Python/Helsinski/part06-12_filtering_file_contents/src/filtering_file_contents.py
@@ -1,1 +1,12 @@
 # Write your solution here
+def filter_solution():
+    with open('solutions.csv', 'r') as new_file:
+        for line in new_file:
+            parts = line.split(";")
+            result[parts[0]] = []
+            for res in parts:
+
+
+
+if __name__ == "__main__":
+    filter_solution()
