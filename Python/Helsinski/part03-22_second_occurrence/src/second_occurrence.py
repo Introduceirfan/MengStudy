fc25e347d7fc846e863b8fe43abd53d336819680
Ini Irfan <febriannaufalirfansyah@gmail.com>
2026-08-13 22:09:46 +0700

    adding 2 problem

--- Python/Helsinski/part03-22_second_occurrence/src/second_occurrence.py
+++ Python/Helsinski/part03-22_second_occurrence/src/second_occurrence.py
@@ -1,1 +1,13 @@
-# Write your solution here
+string = input("Please type in a string: ")
+substring = input("Please type in a substring: ")
+
+first_occurrence = string.find(substring)
+
+if first_occurrence != -1:
+    second_occurrence = string.find(substring, first_occurrence + len(substring))
+    if second_occurrence != -1:
+        print(f"The second occurrence of the substring is at index {second_occurrence}.")
+    else:
+        print("The substring does not occur twice in the string.")
+else:
+        print("The substring does not occur twice in the string.")
--- Python/Helsinski/part06-16_dictionary_file/src/dictionary_file.py
+++ Python/Helsinski/part06-16_dictionary_file/src/dictionary_file.py
@@ -1,1 +1,26 @@
 # Write your solution here
+def dictionary():
+    while True:
+        print("1 - Add word, 2 - Search, 3 - Quit")
+        funct = int(input("Function: "))
+        if funct == 1:
+            word = str(input("The word in Finnish: "))
+            translate = str(input("The word in English:"))
+            with open("dictionary.txt", "a") as new_file:
+                new_file.write(f"{word}:{translate}\n")
+                print("Dictionary entry added")
+
+        elif funct == 2:
+            search = str(input("Search term: "))
+            with open("dictionary.txt", "r") as new_file:
+                for line in new_file:
+                    parts = line.strip().split(":")
+                    if search in parts[0] or search in parts[1]:
+                        print(f"{parts[0]} - {parts[1]}")
+
+        elif funct == 3:
+            print("Bye!")
+            break
+
+
+dictionary()
