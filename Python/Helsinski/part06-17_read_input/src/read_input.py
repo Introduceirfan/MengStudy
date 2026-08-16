3bf54500aba7da563d7925bcec3773bd2b66bdec
Ini Irfan <febriannaufalirfansyah@gmail.com>
2026-08-16 22:15:49 +0700

    adding one problem. more consistency aheadd!!

--- Python/Helsinski/part06-17_read_input/src/read_input.py
+++ Python/Helsinski/part06-17_read_input/src/read_input.py
@@ -1,1 +1,14 @@
 # Write your solution here
+def read_input(str_input, x, y):
+    while True:
+        try:
+            inputs = int(input(str_input))
+            if inputs >= x and inputs <= y:
+                return inputs
+        except:
+            pass
+        print(f"You must type in an integer between {x} and {y}")
+
+if __name__ == "__main__":
+    number = read_input("Please type in a number: ", 5, 10)
+    print("You typed in:", number)
