cf29416e14152d5c7016578b9f5b6e984a32c133
Ini Irfan <febriannaufalirfansyah@gmail.com>
2026-08-17 22:24:46 +0700

    adding 2 problems python

--- Python/Helsinski/part07-01_hypotenuse/src/hypotenuse.py
+++ Python/Helsinski/part07-01_hypotenuse/src/hypotenuse.py
@@ -1,1 +1,9 @@
 # Write your solution here
+from math import sqrt
+def hypotenuse(x,y):
+    hypo = sqrt((x*x) + (y*y))
+    return hypo
+if __name__ == "__main__":
+    print(hypotenuse(3,4)) # 5.0
+    print(hypotenuse(5,12)) # 13.0
+    print(hypotenuse(1,1)) # 1.4142135623730951
--- Python/Helsinski/part07-02_special_characters/src/special_characters.py
+++ Python/Helsinski/part07-02_special_characters/src/special_characters.py
@@ -1,1 +1,22 @@
 # Write your solution here
+import string 
+
+def separate_characters(mystr):
+    letters = ""
+    punctuation = ""
+    others = ""
+    for i in mystr:
+        if i in string.ascii_letters:
+            letters += i
+        elif i in string.punctuation:
+            punctuation += i
+        else:
+            others += i
+
+    return letters, punctuation, others
+ 
+if __name__ == "__main__":
+    parts = separate_characters("Olé!!! Hey, are ümläüts wörking?")
+    print(parts[0])
+    print(parts[1])
+    print(parts[2])
