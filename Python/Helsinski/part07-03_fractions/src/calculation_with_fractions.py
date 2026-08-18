da4b836ed6b288ad3dac0942facf35a9b6d1b686
Ini Irfan <febriannaufalirfansyah@gmail.com>
2026-08-18 20:53:12 +0700

    add 1

--- Python/Helsinski/part07-03_fractions/src/calculation_with_fractions.py
+++ Python/Helsinski/part07-03_fractions/src/calculation_with_fractions.py
@@ -1,1 +1,13 @@
 # Write your solution here
+
+from fractions import Fraction
+
+def fractionate(amount: int) -> list:
+    part = Fraction(1, amount)
+    return [part] * amount
+
+if __name__ == "__main__":
+    for p in fractionate(3):
+        print(p)
+    print()
+    print(fractionate(5))
