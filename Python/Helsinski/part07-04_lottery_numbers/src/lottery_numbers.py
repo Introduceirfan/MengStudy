f81a2ab40ef2511bf9349aa14237014ef0725276
Ini Irfan <febriannaufalirfansyah@gmail.com>
2026-08-19 22:07:37 +0700

    add 1

--- Python/Helsinski/part07-04_lottery_numbers/src/lottery_numbers.py
+++ Python/Helsinski/part07-04_lottery_numbers/src/lottery_numbers.py
@@ -1,1 +1,14 @@
 # Write your solution here
+import random
+
+def lottery_numbers(amount: int, lower: int, upper: int) -> list:
+    numbers = []
+    while len(numbers) < amount:
+        random_num = random.randint(lower, upper)
+        if random_num not in numbers:
+            numbers.append(random_num)
+    numbers.sort()
+    
+    return numbers
+for number in lottery_numbers(7, 1, 40):
+    print(number)
