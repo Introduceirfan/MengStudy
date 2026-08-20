5f3be6fee85692d765baff5d19cd6295331c6fbf
Ini Irfan <febriannaufalirfansyah@gmail.com>
2026-08-20 21:47:49 +0700

    2 prob

--- Python/Helsinski/part06-18_parameter_validation/src/parameter_validation.py
+++ Python/Helsinski/part06-18_parameter_validation/src/parameter_validation.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def new_person(name: str, age: int):
+    if name == "" or len(name.split()) < 2 or len(name) > 40:
+        raise ValueError("Name is not valid")
+    if age < 0 or age >= 150:
+        raise ValueError("Age is not valid")
+
+    return(name, age)
+
+if __name__ == "__main__":
+    new_person("irfan", 19)
--- Python/Helsinski/part07-03_fractions/src/calculation_with_fractions.py
+++ Python/Helsinski/part07-03_fractions/src/calculation_with_fractions.py
@@ -1,13 +1,9 @@
 # Write your solution here
-
 from fractions import Fraction
-
-def fractionate(amount: int) -> list:
+def fractionate (amount: int):
     part = Fraction(1, amount)
     return [part] * amount
 
 if __name__ == "__main__":
     for p in fractionate(3):
         print(p)
-    print()
-    print(fractionate(5))
