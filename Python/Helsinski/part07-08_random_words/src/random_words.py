91f6853eefd10772c1460f441b891261bb761100
Ini Irfan <febriannaufalirfansyah@gmail.com>
2026-08-22 23:06:57 +0700

    holideyy. small learning

--- Python/Helsinski/part07-08_random_words/src/random_words.py
+++ Python/Helsinski/part07-08_random_words/src/random_words.py
@@ -1,1 +1,19 @@
 # Write your solution here
+from random import sample
+def words(n, beginnning):
+    rand_word = []
+    with open("words.txt") as new_file:
+        for line in new_file:
+            word = line.strip()
+            if word.startswith(beginnning):
+                rand_word.append(word)
+
+    if len(rand_word) < n:
+        raise ValueError('bad')
+    
+    return sample(rand_word, n)
+
+if __name__ == "__main__":
+    word_list = words(3, "ca")
+    for word in word_list:
+        print(word)
--- Python/Helsinski/part07-09_how_old/src/how_old.py
+++ Python/Helsinski/part07-09_how_old/src/how_old.py
@@ -1,1 +1,15 @@
 # Write your solution here
+import datetime
+
+day = int(input("Day: "))
+month = int(input("Month: "))
+year = int(input("Year: "))
+age = datetime.date(year, month, day)
+milenium = datetime.date(1999, 12, 31)
+
+
+if age < milenium:
+    difference = milenium - age
+    print(f"You were {difference.days} days old on the eve of the new millennium.")
+else:
+    print("You weren't born yet on the eve of the new millennium.")
